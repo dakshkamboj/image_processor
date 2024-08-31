4fcb8faa487d70461c883a3d127b9731c10bc15d
Daksh <dakshkamboj123@gmail.com>
2024-09-01 00:17:55 +0530

    updated changes

--- processing/urls.py
+++ processing/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UploadView, StatusView
 
 urlpatterns = [
-    path('upload/', UploadView.as_view(), name='upload'),
-    path('status/<str:request_id>/', StatusView.as_view(), name='status'),
+    path('upload', UploadView.as_view(), name='upload'),
+    path('status/<str:request_id>', StatusView.as_view(), name='status'),
 ]
